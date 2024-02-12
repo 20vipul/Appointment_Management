db7d5a306fb1f6f34fa1ebb762224edcde0b1691
20vipul <vipul.singhai1995@gmail.com>
2024-02-12 07:55:15 +0530

    made changes to search.js

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -8,12 +8,12 @@ const DropDown =( {onSortByChange , sortBy, orderBy, onOrderByChange} ) => {
       <DropdownButton
           variant="info"
           title="Sort">
-          <Dropdown.Item href="#" onClick={() =>onSortByChange('firstName')}> First Name {(sortBy == 'firstName')&&  <BsCheck2 className="float-end " />}</Dropdown.Item>
-          <Dropdown.Item href="#" onClick={() =>onSortByChange('lastName')}> Last Name {(sortBy == 'lastName')&&  <BsCheck2 className="float-end " />}</Dropdown.Item>
-          <Dropdown.Item href="#" onClick={() =>onSortByChange('aptDate')}> Date {(sortBy == 'aptDate')&&  <BsCheck2 className="float-end " />}</Dropdown.Item>
+          <Dropdown.Item href="#" onClick={() =>onSortByChange('firstName')}> First Name {(sortBy === 'firstName')&&  <BsCheck2 className="float-end " />}</Dropdown.Item>
+          <Dropdown.Item href="#" onClick={() =>onSortByChange('lastName')}> Last Name {(sortBy === 'lastName')&&  <BsCheck2 className="float-end " />}</Dropdown.Item>
+          <Dropdown.Item href="#" onClick={() =>onSortByChange('aptDate')}> Date {(sortBy === 'aptDate')&&  <BsCheck2 className="float-end " />}</Dropdown.Item>
           <Dropdown.Divider />
-          <Dropdown.Item href="#" onClick={() =>onOrderByChange('asc')}> Ascending {(orderBy == 'asc')&&  <BsCheck2 className="float-end " />}</Dropdown.Item>
-          <Dropdown.Item href="#" onClick={() =>onOrderByChange('desc')}> Descinding {(orderBy == 'desc')&&  <BsCheck2 className="float-end " />}</Dropdown.Item>
+          <Dropdown.Item href="#" onClick={() =>onOrderByChange('asc')}> Ascending {(orderBy === 'asc')&&  <BsCheck2 className="float-end " />}</Dropdown.Item>
+          <Dropdown.Item href="#" onClick={() =>onOrderByChange('desc')}> Descinding {(orderBy === 'desc')&&  <BsCheck2 className="float-end " />}</Dropdown.Item>
         </DropdownButton>
     </>
   );
